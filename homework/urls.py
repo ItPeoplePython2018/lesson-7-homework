33266f91ab16d6fb82790b3394db4e707410d845
iminlan <38048764+iminlan@users.noreply.github.com>
2018-07-27 22:27:17 +0500

    Update urls.py

--- homework/urls.py
+++ homework/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from homework.app.views import start_page, pages_list, categories, pages_with_unknown_category
 
 urlpatterns = [
+    path('', start_page),
+    path('pages/', pages_list),
+    path('category/<int:id>/', categories),
+    path('category/unknown/', pages_with_unknown_category),
     path('admin/', admin.site.urls),
 ]
+
